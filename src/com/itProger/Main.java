fe726c8f6b149dad4425fccb51d54ecb0a21c394
AndreevIP <Andreev2102@inbox.ru>
2019-08-21 16:05:27 +0300

    Последний вариант задачи 13

--- src/com/itProger/Main.java
+++ src/com/itProger/Main.java
@@ -7,21 +7,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите первую строку: " );
+        System.out.println("Введите первую строку: ");
         String str1 = scanner.nextLine();
 
         System.out.println("Введите вторую строку: ");
         String str2 = scanner.nextLine();
 
-        if (str1.length() > str2.length()){
-            System.out.println("Строка с наибольшей длинной: " + str1);
-        }
-        else if (str1.length() < str2.length()){
-            System.out.println("Строка с наибольшей длинной: " + str2);
-        }
-        else{
-            System.out.println("Длина 1-ой строки равна длине 2-ой строки");
-        }
-    }
-
+        System.out.println(str1.length() == str2.length() ? "Длина 1-ой строки равна длине 2-ой строки" : str1.length() < str2.length() ? "Строка с наибольшей длинной: " + str2 : "Строка с наибольшей длинной: " + str1);
+//
+//        if (str1.length() > str2.length()) {
+//            System.out.println("Строка с наибольшей длинной: " + str1);
+//        } else if (str1.length() < str2.length()) {
+//            System.out.println("Строка с наибольшей длинной: " + str2);
+//        } else {
+//            System.out.println("Длина 1-ой строки равна длине 2-ой строки");
+//        }
+   }
 }
+
